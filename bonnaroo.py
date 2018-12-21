c271ffdd6d68cf9ee83596214f8df52879d79c87
Eric Thomas <thomas.3448@osu.edu>
2018-12-20 21:34:45 -0500

    Update bonnaroo.py
    sys.argv[2] must now be username
    update scope and username variables to log into spotify

--- bonnaroo.py
+++ bonnaroo.py
@@ -3,11 +3,13 @@ import sys
 import spotipy.util as util
 #Michael's Comment
 def main():
-	token = util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
 	file = open(sys.argv[1], "r")
 	artists = [artist for artist in file]
 	playlist_name = "Bonnaroo Playlist 2019"
 	playlist_description = "Barns, beats, Bonnaroo. Get ready for 2019 with these tunes as hot as the Tennessee sun!"
+	username = sys.argv[2]
+	scope = "playlist-modify-public"
+	token = util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
 	if token:
 		sp = spotipy.Spotify(auth=token)
 		playlists = sp.user_playlist_create(username, playlist_name,playlist_description)
