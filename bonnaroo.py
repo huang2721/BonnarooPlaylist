4c9fc7c5060fca2d09528d626bd381fd80a3cd32
Eric Thomas <thomas.3448@osu.edu>
2018-12-21 10:13:08 -0500

    Playlist name check
    Implemented a method to check to see if playlist "Bonnaroo Playlist 2019" is already in the users playlists. If it is, nothing happens, and if its not then it is created.

--- bonnaroo.py
+++ bonnaroo.py
@@ -14,19 +14,29 @@ def main():
 	if token:
 		# Create spotify object
 		sp = spotipy.Spotify(auth=token)
-		# Create playlist
-		playlist = sp.user_playlist_create(username,playlist_name)
-		# Get IDs of all artists in the text file
-		artistIDs = get_artist_ids(sp, artists)
-		# Grab top 5 songs and add them to playlist
-		songIDs = [sp.artist_top_tracks(artistIDs[i])['tracks'][j]['id'] for i in range(len(artistIDs)) for j in range(5)]
-		sp.user_playlist_add_tracks(username, playlist['id'], songIDs)
+		# Check to see if Bonnaroo playlist already exists
+		userPlaylists = get_user_playlist_names(sp)
+		if playlist_name not in userPlaylists:
+			# Create playlist
+			playlist = sp.user_playlist_create(username,playlist_name)
+			# Get IDs of all artists in the text file
+			artistIDs = get_artist_ids(sp, artists)
+			# Grab top 5 songs and add them to playlist
+			songIDs = [sp.artist_top_tracks(artistIDs[i])['tracks'][j]['id'] for i in range(len(artistIDs)) for j in range(5)]
+			sp.user_playlist_add_tracks(username, playlist['id'], songIDs)
+		else:
+			print("Bonnaroo Playlist 2019 has already been created :)")
 	else:
 		print("Can't get token for", username)
 
 def get_artist_ids(sp, artists):
 	artistIDs = [sp.search(artist,1,0,"artist")['artists']['items'][0]['id'] for artist in artists]
 	return artistIDs
+
+
+def get_user_playlist_names(sp):
+	userPlaylists = sp.current_user_playlists()
+	return [userPlaylists['items'][i]['name'] for i in range(len(userPlaylists['items']))]
 
 if __name__ == '__main__':
 	main()
