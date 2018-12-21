5aa5d257bde38ad972fe33113943db8a1a58cc7e
Daniy Lomeo <lomeo.7@osu.edu>
2018-12-20 20:07:11 -0500

    Added some imports and token stuff
    read above

--- bonnaroo.py
+++ bonnaroo.py
@@ -1,10 +1,16 @@
 import spotipy
 import sys
+import spotipy.util as util
 #Michael's Comment
 def main():
-	util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
+	token = util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
 	file = open(sys.argv[1], "r")
 	artists = [artist for artist in file]
+	if token:
+		sp = spotipy.Spotify(auth=token)
+
+
+
 
 if __name__ == '__main__':
 	main()
