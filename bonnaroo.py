da004b92069dd999bca3e7ab485f30a721ac5aed
Eric Thomas <thomas.3448@osu.edu>
2018-12-20 21:22:18 -0500

    Username is now sys.argv[2]

--- bonnaroo.py
+++ bonnaroo.py
@@ -6,6 +6,7 @@ def main():
 	file = open(sys.argv[1], "r")
 	artists = [artist for artist in file]
 	playlist_name = "Bonnaroo Playlist 2019"
+	username = sys.argv[2]
 	token = util.prompt_for_user_token(username)
 	print (token)
 	if token:
