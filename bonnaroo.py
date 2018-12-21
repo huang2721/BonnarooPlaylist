77ae0f482a01b7932941f279f40c2159779b0e34
Daniy Lomeo <lomeo.7@osu.edu>
2018-12-20 20:25:59 -0500

    prints playlist (hopefully id)

--- bonnaroo.py
+++ bonnaroo.py
@@ -3,19 +3,18 @@ import sys
 import spotipy.util as util
 #Michael's Comment
 def main():
+	token = util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
 	file = open(sys.argv[1], "r")
 	artists = [artist for artist in file]
 	playlist_name = "Bonnaroo Playlist 2019"
-	username = sys.argv[2]
-	token = util.prompt_for_user_token(username)
-	print (token)
-	if token:
-		sp = spotipy.Spotify(auth=token)
-		print (token)
 	playlist_description = "Barns, beats, Bonnaroo. Get ready for 2019 with these tunes as hot as the Tennessee sun!"
 	if token:
 		sp = spotipy.Spotify(auth=token)
-		sp.user_playlist_create(username, playlist_name,playlist_description)
+		playlists = sp.user_playlist_create(username, playlist_name,playlist_description)
+		print(playlists)
+
+
+
 
 if __name__ == '__main__':
 	main()
