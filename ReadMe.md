e8a7717e4b62376eb6012eb2222f7be899ac963c
Michael Huang <huang.2721@buckeyemail.osu.edu>
2018-12-21 08:31:02 -0500

    Edits
    Made changes to bonnaroo.py & readme

--- ReadMe.md
+++ ReadMe.md
@@ -2,6 +2,9 @@ Python script written by Michael Huang, Daniel Lomeo, and Eric Thomas
 
 Type "pip install spotipy" to collect spotipy dependency
 
-To run type "python bonnaroo.py"
+To run, open Windows Powershell
+1. Type "python -m http.server".
+2. Open another instance of Windows Powershell.
+3. Type "python bonnaroo.py [Spotify Username]" where [Spotify Username] should be replaced with the user's Spotify username.
 
 This program makes a spotify playlist of the top 5 songs of all the bonnaroo artists for 2019.
--- bonnaroo.py
+++ bonnaroo.py
@@ -3,16 +3,16 @@ import sys
 import spotipy.util as util
 
 def main():
-	file = open(sys.argv[1], "r")
+	file = open("artists.txt", "r")
 	artists = [artist for artist in file]
 	playlist_name = "Bonnaroo Playlist 2019"
 	playlist_description = "Barns, beats, Bonnaroo. Get ready for 2019 with these tunes as hot as the Tennessee sun!"
-	username = sys.argv[2]
+	username = sys.argv[1]
 	scope = "playlist-modify-public"
 	token = util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://startbackpacking.org')
 	if token:
 		sp = spotipy.Spotify(auth=token)
-		sp.trace=False
+		sp.trace = False
 		public = True
 		playlist = sp.user_playlist_create(username,playlist_name,public)
 		id=playlist['id']
