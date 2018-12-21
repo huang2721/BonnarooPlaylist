ab64cb4d58c74e1c865fb802aa23189e374a729c
Michael Huang <huang.2721@buckeyemail.osu.edu>
2018-12-20 20:56:38 -0500

    Added app credentials

--- bonnaroo.py
+++ bonnaroo.py
@@ -2,6 +2,7 @@ import spotipy
 import sys
 #Michael's Comment
 def main():
+	util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
 	file = open(sys.argv[1], "r")
 	artists = [artist for artist in file]
 
