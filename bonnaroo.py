c69f66323d378664f3cb19565517f6ad953a1302
Eric Thomas <thomas.3448@osu.edu>
2018-12-20 21:17:12 -0500

    Token update

--- bonnaroo.py
+++ bonnaroo.py
@@ -3,12 +3,14 @@ import sys
 import spotipy.util as util
 #Michael's Comment
 def main():
-	token = util.prompt_for_user_token(username,scope,client_id='52e761dfa7e542b69f9250cb7d243bca',client_secret='30de97fa9ae24103ac067dcf1683dce2',redirect_uri='http://localhost:8888/callback')
 	file = open(sys.argv[1], "r")
 	artists = [artist for artist in file]
 	playlist_name = "Bonnaroo Playlist 2019"
+	token = util.prompt_for_user_token(username)
+	print (token)
 	if token:
 		sp = spotipy.Spotify(auth=token)
+		print (token)
 
 
 
