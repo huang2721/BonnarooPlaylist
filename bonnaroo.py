3eec67327c4402487919f5c227f7b83832f1779a
Eric Thomas <thomas.3448@osu.edu>
2018-12-21 12:39:36 -0500

    Update bonnaroo.py

--- bonnaroo.py
+++ bonnaroo.py
@@ -17,26 +17,13 @@ def main():
 		# Get IDs of all artists in the text file
 		artistIDs = get_artist_ids(sp, artists)
 		# Grab list of top 5 songs for each artist (No remixes for certain genres)
-		remixesAllowed = ['edm', 'electro', 'electronic']
+		remixesAllowed = []
 		songIDs = get_song_IDs(sp, artistIDs, remixesAllowed)
-
-		"""
-		# Check to see if Bonnaroo playlist already exists
-		userPlaylists = get_user_playlist_names(sp)
-		if playlist_name not in userPlaylists:
-			# Create playlist
-			playlist = sp.user_playlist_create(username,playlist_name)
-			# Adds top 5 songs for each artist to playlist
-			add_track(sp, username, playlist, songIDs)
-		else:
-			print("Bonnaroo Playlist 2019 has already been created :)")
-			"""
-
 		# Create playlist
 		playlist = sp.user_playlist_create(username, playlist_name)
 		# Adds top 5 songs for each artist to playlist
 		add_track(sp, username, playlist, songIDs)
-	
+		print("Playlist was created successfully :) <3")
 	else:
 		print("Can't get token for", username)
 
@@ -47,12 +34,6 @@ def get_artist_ids(sp, artists):
 		artistGenre = sp.search(artist,1,0,"artist")['artists']['items'][0]['genres']
 		artistIDs[artistID] = artistGenre
 	return artistIDs
-
-"""
-def get_user_playlist_names(sp):
-	userPlaylists = sp.current_user_playlists()
-	return [userPlaylists['items'][i]['name'] for i in range(len(userPlaylists['items']))]
-	"""
 
 def get_song_IDs(sp, artistIDs, remixesAllowed):
 	songIDs = []
